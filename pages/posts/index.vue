43a916180879b73e43d69d8bfbdafa20e0032f6f
Rulo Kobashikawa <akobashikawa@gmail.com>
2018-12-06 18:01:35 -0500

    First commit

---
+++ pages/posts/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="posts-page">
+    <h1>Posts</h1>
+  </div>
+</template>
+
+<style scoped>
+.post-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
